38b5f3c3b1e9c1c1235038d5c0bbd838e4c81059
Julian Ladjani <julian.ladjani@epitech.eu>
2017-02-01 17:39:40 +0100

    LOT OF MODIF

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@
 ** Login   <maxime.picot@epitech.net>
 ** 
 ** Started on  Tue Jan 31 15:14:08 2017 Maxime PICOT
-** Last update Tue Jan 31 15:14:32 2017 Maxime PICOT
+** Last update Wed Feb  1 17:09:01 2017 julian ladjani
 */
 
+int	main(int ac, char **av, char **ae)
+{
+  prepare_my_signal();
+}
--- src/my_signal.c
+++ src/my_signal.c
@@ -5,15 +5,69 @@
 ** Login   <julian.ladjani@epitech.net>
 ** 
 ** Started on  Tue Jan 31 16:38:41 2017 julian ladjani
-** Last update Tue Jan 31 16:45:33 2017 julian ladjani
+** Last update Wed Feb  1 17:25:05 2017 julian ladjani
 */
 
-void	handler_usr1(int sig, siginfo_t *si, void *unised)
+void			handler_usr1(int sig, siginfo_t *si, void *unised)
 {
-  
+  if (game.mode == 0)
+    kill(game.opid, SIGUSR1);
+  if (game.mode == 1)
+    game.posx++;
+  if (game.mode == 2)
+    game.posy++;
 }
 
-void	handler_usr2(int sig, siginfo_t *si, void *unised)
+void			handler_usr2(int sig, siginfo_t *si, void *unised)
+{ 
+  if (game.mode <= 1)
+    game.mode++;
+  else
+    game.mode = 0;
+}
+
+void			my_sendpos()
 {
-  
+  int			x;
+  int			y;
+
+  x = 0;
+  y = 0;
+  kill(game.opid, SIGUSR2);
+  usleep(50);
+  while (x < game.posx)
+    {
+      kill(game.opid, SIGUSR1);
+      x++;
+      usleep(50);
+    }
+  kill(game.opid, SIGUSR2);
+  usleep(50);
+  while (y < game.posy)
+    {
+      kill(game.opid, SIGUSR1);
+      y++;
+      usleep(50);
+    }
+  kill(game.opid, SIGUSR2);
+}
+
+void			my_sendidle()
+{
+  kill(game.opid, SIGUSR1);
+}
+
+void			prepare_my_signal()
+{
+  struct sigaction      sa;
+  struct sigaction	sb;
+
+  sa.sa_flags = SA_SIGINFO;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_sigaction = handler_usr1;
+  sb.sa_flags = SA_SIGINFO;
+  sigemptyset(&sb.sa_mask);
+  sb.sa_sigaction = handler_usr2;
+  sigaction(SIGUSR1, &sa, NULL);
+  sigaction(SIGUSR2, &sb, NULL);
 }
